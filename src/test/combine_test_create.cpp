72e5fb9babd6c86be548db4e3b885fdf09bba9c1
Frank T. Bergmann <fbergman@caltech.edu>
2016-08-16 14:04:10 +0200

    - added more tests

--- src/test/combine_test_create.cpp
+++ src/test/combine_test_create.cpp
@@ -5,25 +5,59 @@
 #include <zipper/tools.h>
 
 #include <combine/combinearchive.h>
+#include <combine/knownformats.h>
 #include <omex/CaOmexManifest.h>
 
 #include <vector>
 #include <fstream>
 #include <ostream>
+#include <sstream>
 #include <string>
 #include <map>
+#include <cstdlib>
+
+/**
+ * Tries to find the test file in the srcdir environment variable.
+ *
+ * @param fileName the filename relative to this tests srcdir
+ *        environment variable.
+ *
+ * If the filename cannot be found, the test from which this function
+ * is called will fail.
+ *
+ * @return the full path to the test file
+ */
+std::string getTestFile(const std::string& fileName)
+{
+  std::stringstream str;
+  char* srcDir = getenv("srcdir");
+  
+  if (srcDir != NULL) str << srcDir;
+  else str << ".";
+
+  str << "/" << fileName;
+  std::string fullName = str.str();
+
+  if (!checkFileExists(fullName))
+  {
+    FAIL("The test file '" << fileName << "' could not be found. please specify the 'srcdir' environment variable. The full path tried was: " << fullName);
+  }
+
+  return fullName;
+}
+
+
 
 SCENARIO("creating a new combine archive", "[combine]")
 {
   GIVEN("a user creates a new archive")
   {
     CombineArchive archive;
-    REQUIRE(!archive.getBaseDir().empty());
     REQUIRE(archive.getManifest() == NULL);
 
     WHEN("a document is added")
     {
-      archive.addFile("E:\\Users\\fbergmann\\Documents\\SBML Models\\BorisEJB.xml",
+      archive.addFile(getTestFile("test-data/BorisEJB.xml"),
                       "./model/BorisEJB.xml",
                       "http://identifiers.org/combine.specifications/sbml");
 
@@ -35,26 +69,77 @@ SCENARIO("creating a new combine archive", "[combine]")
         REQUIRE(entry != NULL);
         REQUIRE(entry->getLocation() == "./model/BorisEJB.xml");
         REQUIRE(entry->getFormat() == "http://identifiers.org/combine.specifications/sbml");
+        REQUIRE(entry->isFormat("sbml"));
 
-        AND_WHEN("the archive is saved")
+        std::string modelContent = archive.extractEntryToString("./model/BorisEJB.xml");
+        REQUIRE(!modelContent.empty());
+
+        AND_WHEN("metadata is added")
         {
-          // ensure that the file is not already present, and if so remove it
-          if (checkFileExists("out.omex"))
-            std::remove("out.omex");
+          VCard creator;
+          creator.setEmail("fbergman@caltech.edu");
+          creator.setFamilyName("Bergmann");
+          creator.setGivenName("Frank");
+          creator.setOrganization("Caltech");
 
-          archive.writeToFile("out.omex");
+          Date current = OmexDescription::getCurrentDataAndTime();
 
-          CombineArchive second;
-          second.initializeFromArchive("out.omex");
+          OmexDescription desc;
+          desc.setAbout(".");
+          desc.setDescription("great archive");
+          desc.setCreated(current);
+          desc.addModification(current);
+          desc.addCreator(creator);
 
-          THEN("it can be loaded and the files are present.")
+          archive.addMetadata(".", desc);
+
+
+          THEN("the metadata is accessible")
           {
-            REQUIRE(second.getManifest() != NULL);
-            REQUIRE(second.getManifest()->getNumContents() == 1);
-            entry = second.getManifest()->getContent(0);
-            REQUIRE(entry != NULL);
-            REQUIRE(entry->getLocation() == "./model/BorisEJB.xml");
-            REQUIRE(entry->getFormat() == "http://identifiers.org/combine.specifications/sbml");
+
+            OmexDescription desc2 = archive.getMetadataForLocation(".");
+            REQUIRE(!desc2.isEmpty());
+            REQUIRE(desc2.getAbout() ==".");
+            REQUIRE(desc2.getDescription() == "great archive");
+            REQUIRE(desc2.getCreated().getDateAsString() == current.getDateAsString());
+            REQUIRE(desc2.getNumCreators() == 1);
+            REQUIRE(!desc2.getCreator(0).isEmpty());
+
+
+            AND_WHEN("the archive is saved")
+            {
+              // ensure that the file is not already present, and if so remove it
+              if (checkFileExists("out.omex"))
+                std::remove("out.omex");
+
+              archive.writeToFile("out.omex");
+
+              CombineArchive second;
+              second.initializeFromArchive("out.omex");
+
+              THEN("it can be loaded and the files are present.")
+              {
+                REQUIRE(second.getManifest() != NULL);
+                REQUIRE(second.getManifest()->getNumContents() == 1);
+
+                OmexDescription desc3 = archive.getMetadataForLocation(".");
+                REQUIRE(!desc3.isEmpty());
+                REQUIRE(desc3.getAbout() == ".");
+                REQUIRE(desc3.getDescription() == "great archive");
+                REQUIRE(desc3.getCreated().getDateAsString() == current.getDateAsString());
+                REQUIRE(desc3.getNumCreators() == 1);
+                REQUIRE(!desc3.getCreator(0).isEmpty());
+
+                entry = second.getManifest()->getContent(0);
+                REQUIRE(entry != NULL);
+                REQUIRE(entry->getLocation() == "./model/BorisEJB.xml");
+                REQUIRE(entry->getFormat() == "http://identifiers.org/combine.specifications/sbml");
+
+                std::string modelContent = second.extractEntryToString("./model/BorisEJB.xml");
+                REQUIRE(!modelContent.empty());
+
+              }
+            }
           }
         }
       }
