33bc67bad23411a37a6f9ac6de299546363f23f2
Frank T. Bergmann <fbergman@caltech.edu>
2016-08-16 16:21:35 +0200

    - added examples

---
+++ examples/csharp/CreateArchiveExample.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using libcombinecs;
+
+namespace CreateArchiveExample
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var archive = new CombineArchive();
+      archive.addFile(
+            @"e:\users\fbergmann\documents\sbml models\borisejb.xml", // filename
+            "./models/boris.xml", // target file name
+            KnownFormats.lookupFormat("sbml"), // look up identifier for SBML models
+            true // mark file as master
+            );
+
+      var description = new OmexDescription();
+      description.setAbout("."); // about the archive itself
+      description.setDescription("Simple test archive including one SBML model");
+      description.setCreated(OmexDescription.getCurrentDateAndTime());
+
+      var creator = new VCard();
+      creator.setFamilyName("Bergmann");
+      creator.setGivenName("Frank");
+      creator.setEmail("fbergman@caltech.edu");
+      creator.setOrganization("Caltech");
+
+      description.addCreator(creator);
+
+      archive.addMetadata(".", description);
+
+      archive.writeToFile("out.omex");
+    }
+  }
+}
