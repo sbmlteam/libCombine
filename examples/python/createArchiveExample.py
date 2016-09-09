ee659bfc0ca7db69fe7999d652310873fa8f8b16
Frank T. Bergmann <fbergman@caltech.edu>
2016-09-09 14:48:15 +0200

    - add python example

---
+++ examples/python/createArchiveExample.py
@@ -0,0 +1,39 @@
+﻿import sys
+
+from libcombine import *
+
+def createArchiveExample(fileName):
+  
+  archive = CombineArchive();
+  archive.addFile(
+    fileName, # filename
+    "./models/model.xml", # target file name
+    KnownFormats.lookupFormat("sbml"), # look up identifier for SBML models
+    True # mark file as master
+    );
+  
+  description = OmexDescription();
+  description.setAbout("."); # about the archive itself
+  description.setDescription("Simple test archive including one SBML model");
+  description.setCreated(OmexDescription.getCurrentDateAndTime());
+  
+  creator = VCard();
+  creator.setFamilyName("Bergmann");
+  creator.setGivenName("Frank");
+  creator.setEmail("fbergman@caltech.edu");
+  creator.setOrganization("Caltech");
+  
+  description.addCreator(creator);
+  
+  archive.addMetadata(".", description);
+  
+  archive.writeToFile("out.omex");
+
+
+if __name__ == "__main__":
+  if len(sys.argv) < 2:
+    print ("usage createArchiveExample sbml-file")
+    sys.exit(1)
+    
+  createArchiveExample(sys.argv[1])
+
