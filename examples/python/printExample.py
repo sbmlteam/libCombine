6770ddd46a4321fb3aa737653b432694a39aa2cd
Matthias Koenig <konigmatt@googlemail.com>
2017-09-01 11:23:42 +0200

    examples documented, pep8 fixes, print function fixes, tests

--- examples/python/printExample.py
+++ examples/python/printExample.py
@@ -1,56 +1,73 @@
-﻿import sys
+﻿#!/usr/bin/env python
+"""
+Example prints contents of combine archive.
+"""
+from __future__ import print_function
+import sys
 
-from libcombine import *
+try:
+    from libcombine import *
+except ImportError:
+    # fix as long as there is no pip package: see https://github.com/sbmlteam/libCombine/issues/10
+    from tecombine import *
+
 
 def printMetaDataFor(archive, location):
-  desc = archive.getMetadataForLocation(location);
-  if desc.isEmpty():
-  {
-    print("  no metadata for '{0}'".format(location));
-    return;
+    """ Prints metadata for given location.
 
-  print("  metadata for '{0}':".format(location));
-  print("     Created : {0}".format(desc.getCreated().getDateAsString()));
-  for i in range(desc.getNumModified()):
-    print("     Modified : {0}".format(desc.getModified(i).getDateAsString()));
+    :param archive: CombineArchive instance
+    :param location:
+    :return:
+    """
+    desc = archive.getMetadataForLocation(location)
+    if desc.isEmpty():
+        print("  no metadata for '{0}'".format(location))
+        return None
 
-  print("     # Creators: {0}".format(desc.getNumCreators()));
-  for i in range(desc.getNumCreators()):
-    creator = desc.getCreator(i);
-    print("       {0} {1}".format(creator.getGivenName(), creator.getFamilyName()));
+    print("  metadata for '{0}':".format(location))
+    print("     Created : {0}".format(desc.getCreated().getDateAsString()))
+    for i in range(desc.getNumModified()):
+        print("     Modified : {0}".format(desc.getModified(i).getDateAsString()))
+
+    print("     # Creators: {0}".format(desc.getNumCreators()))
+    for i in range(desc.getNumCreators()):
+        creator = desc.getCreator(i)
+        print("       {0} {1}".format(creator.getGivenName(), creator.getFamilyName()))
+
 
 def printArchive(fileName):
-  
-  archive = CombineArchive();
-  if !archive.initializeFromArchive(fileName):
-  {
-    System.out.println("Invalid Combine Archive");
-    return;
-  }
-  
-  printMetaDataFor(archive, ".");
-  
-  print("Num Entries: {0}".format(archive.getNumEntries()));
-  
-  for i in range (archive.getNumEntries()):
-    entry = archive.getEntry(i);
-    
-    print(" {0}: location: {1} format: {2}".format(i, entry.getLocation(), entry.getFormat()));
-    printMetaDataFor(archive, entry.getLocation());
-  
-    # the entry could now be extracted via 
-    # archive.extractEntry(entry.getLocation(), <filename or folder>)
-  
-    # or used as string
-    # content = archive.extractEntryToString(entry.getLocation());
-  
-  
-  archive.cleanUp();
+    """ Prints content of combine archive
+
+    :param fileName: path of archive
+    :return: None
+    """
+    archive = CombineArchive()
+    if archive.initializeFromArchive(fileName) is None:
+        print("Invalid Combine Archive")
+        return None
+
+    print('*'*80)
+    print('Print archive')
+    print('*' * 80)
+    printMetaDataFor(archive, ".")
+    print("Num Entries: {0}".format(archive.getNumEntries()))
+
+    for i in range(archive.getNumEntries()):
+        entry = archive.getEntry(i)
+        print(" {0}: location: {1} format: {2}".format(i, entry.getLocation(), entry.getFormat()))
+        printMetaDataFor(archive, entry.getLocation())
+
+        # the entry could now be extracted via
+        # archive.extractEntry(entry.getLocation(), <filename or folder>)
+
+        # or used as string
+        # content = archive.extractEntryToString(entry.getLocation());
+
+    archive.cleanUp()
+
 
 if __name__ == "__main__":
-  if len(sys.argv) < 2:
-    print ("usage printArchive archive-file")
-    sys.exit(1)
-    
-  printArchive(sys.argv[1])
-
+    if len(sys.argv) < 2:
+        print("usage: python printExample.py archive-file")
+        sys.exit(1)
+    printArchive(sys.argv[1])
